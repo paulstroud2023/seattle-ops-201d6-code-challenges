fe6fa4b3ef178cac201d06948088a79c3f592f93
Paul Stroud <paul.stroud2023@gmail.com>
2023-02-10 01:22:31 +0000

    uploading challenge #4

---
+++ 04/04. array.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Paul Stroud
+# Date of latest revision: 02/08/23
+# Purpose: Basic script functions
+#
+# Resources used: google, https://devhints.io/bash
+
+# Main
+
+test=() # initialize an empty array
+for i in {1..4}; do # a short loop to create dir names
+    test+=("dir"$i) # concatenate string+number
+done
+
+# echo ${test[@]} # confirm the array contents
+
+
+echo "Creating directories in current path; adding a text file inside each:"
+for i in ${test[@]}; do # iterate over the array
+    echo -n $i" ... "   # print array element message
+    mkdir $i    # create dir using array element
+    echo abc123 > $i"/test.txt"  # create a new text file containing "abc123"
+    echo "DONE!"
+done
+
+
+echo "Aaaaand it's gone!! It's all gone."
+# End
