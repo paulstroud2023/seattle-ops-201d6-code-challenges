3f65ff0859727561fd0b9649929d7fd47442a5ec
Paul Stroud <paul.stroud2023@gmail.com>
2023-02-14 23:26:23 +0000

    added dir functionality

--- 06/06. conditionals.sh
+++ 06/06. conditionals.sh
@@ -2,7 +2,7 @@
 
 # Script: Ops 201 Class 06 Ops Challenge Solution
 # Author: Paul Stroud
-# Date of latest revision: 02/13/23
+# Date of latest revision: 02/14/23
 # Purpose: Conditionals
 #
 # Resources used: google, https://devhints.io/bash, https://linuxize.com/post/bash-check-if-file-exists/
@@ -13,11 +13,29 @@ echo "< Code Challenge 06 >"
 echo -n "Enter file name: "
 read FNAME  # read user input
 
-if [[ -f $FNAME ]]  #check if file exists
-  then echo "The file" $FNAME "already exists!"    
-  else
-    echo -n "Creating " $FNAME "..."
-    touch $FNAME && echo "DONE!"
+# a while loop to validate user input
+TYPE=""
+while [[ $TYPE != "f" && $TYPE != "d" ]]; do
+    echo -n "Enter 'f' for file or 'd' for dir: "
+    read TYPE  # read user input
+done
+
+# $TYPE is either 'f' or 'd'
+if [[ $TYPE == "f" ]]  # if user chose 'f'
+  then  # test/create a file
+    if [[ -f $FNAME ]]  #check if file exists
+    then echo "The file" $FNAME "already exists!"    
+    else
+        echo -n "Creating " $FNAME "..."
+        touch $FNAME && echo "DONE!"
+    fi
+  else  # otherwise test/create a dir
+    if [[ -d $FNAME ]]  #check if dir exists
+      then echo "The dir" $FNAME "already exists!"    
+      else
+        echo -n "Creating " $FNAME "..."
+        mkdir $FNAME && echo "DONE!"
+    fi
 fi
 
 
