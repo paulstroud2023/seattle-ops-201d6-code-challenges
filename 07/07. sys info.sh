2aedae91e6dcd43c21e0bfad79db932e6c960aff
Paul Stroud <paul.stroud2023@gmail.com>
2023-02-14 23:02:33 +0000

    added comments

--- 07/07. sys info.sh
+++ 07/07. sys info.sh
@@ -11,9 +11,10 @@
 
 echo "*** Le Compooter ***"
 
-if (( $(id -u) == 0 ))
+# check for root access
+if (( $(id -u) == 0 ))  # UID == 0 is root
   then echo "Root access check: OK"
-  else 
+  else  # exit the script
         echo "Root access check: FAILED"
         echo "Please re-run this script as sudo/root"
         exit
@@ -21,22 +22,24 @@ fi
 
 
 echo -e "\n> COMPUTER NAME:"
-lshw | head -n 3
+lshw | head -n 3  # first three lines containing computer name, etc.
 
 echo -e "\n> CPU INFO:"
-lshw -class cpu | grep -v capa
+lshw -class cpu | grep -v capa  # CPU info excluding the "capabilities" section
 
 echo -e "\n> RAM INFO:"
-lshw -class memory | grep -A 3 "memory$"
+# RAM info, grep for the section that "memory" at EOL, print 3 lines
+lshw -class memory | grep -A 3 "memory$"  
 
 echo -e "\n> DISPLAY ADAPTER INFO:"
-lshw -class display
+lshw -class display   # diplay adapter hw info
 
 echo -e "\n> NETWORK ADAPTER INFO:"
-lshw -class network
+lshw -class network   # network adapter hw info
 
 echo -e "\n> BIOS INFO:"
-dmidecode -H 0000 | tail -n +9
+# pull only Handle 0x0000, skip the first 9 lines
+dmidecode -H 0000 | tail -n +9  
 
 
 echo -e "\n*** Mission complete! ***\n"
