339e4fce5b3454d63e84e823041107e7c41fdc76
Paul Stroud <124333025+paulstroud2023@users.noreply.github.com>
2023-02-21 11:34:07 -0800

    Add files via upload

--- 12/12. powershell ip analysis.ps1
+++ 12/12. powershell ip analysis.ps1
@@ -7,23 +7,32 @@
 
 # Main
 
-function grab_ipv4_string
+function grab_ipv4_string # extract the line containing IPv4 from ipconfig output
         {
          return ipconfig /all | Select-String -Pattern "ipv4" | out-string
         }
+
+function test_ping # test connectivity
+        {
+         return Test-Connection 8.8.8.8 -Quiet
+        }
                 
 
-$ping_test = "FAIL"
-
-
-
-#$ip_addr = ipconfig /all | Select-String -Pattern "ipv4" | out-string
-$ip_addr = grab_ipv4_string
+$ip_addr = grab_ipv4_string # save function output to a var
 # extract the IPv4 address from the string via regex match -> select second element in array
 $ip_addr = $ip_addr.split(":|\(") | Select-Object -index 1
 $ip_addr = $ip_addr.split(" ") # remove the leading space
 
+echo "Extracted IPv4: "
 echo $ip_addr
 
+echo "Testing network connection ... "
+
+$ping_test = "FAIL"
+if (test_ping) { $ping_test = "OK!" }
+
+echo $ping_test
+
+echo ""
 echo "Job's done!"
 # End
